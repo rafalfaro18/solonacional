04428a49d490c074c79bc3056533225585d94d32
Rafael Alfaro <rafalfaro_18@outlook.com>
2016-11-09 20:46:06 -0600

    Carga el carrusel al terminar de cargar la pagina

--- js/carousel-custom.js
+++ js/carousel-custom.js
@@ -1,3 +1,8 @@
+$(window).load(function() {
+     // + any other carousel related stuff that has to wait for the images to complete loading
+     $('.carousel').carousel();
+});
+
 $(function () {
     $('#myCarousel').carousel({
         interval:5000,
